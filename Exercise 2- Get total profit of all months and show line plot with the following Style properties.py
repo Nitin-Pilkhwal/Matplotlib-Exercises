19b8874b448a311d81355dce18f1d5e257ef299e
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-03-11 01:04:26 +0530

    Create Exercise 2- Get total profit of all months and show line plot with the following Style properties.py

---
+++ Exercise 2- Get total profit of all months and show line plot with the following Style properties.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#importing dataset which is saved locally
+df = pd.read_csv('company_sales_data.csv')
+
+#Exercise 2
+# Generated line plot must include following Style properties: –
+
+# Line Style dotted and Line-color should be red
+# Show legend at the lower right location.
+# X label name = Month Number
+# Y label name = Sold units number
+# Add a circle marker.
+# Line marker color as red
+# Line width should be 3
+
+
+
+#Solution
+
+columns = ["month_number","facecream","facewash","toothpaste","bathingsoap","shampoo","moisturizer","total_units","total_profit"]
+
+
+#plotting the graph
+plt.figure(figsize=(5,5))
+
+plt.plot(df['month_number'],df['total_units'],c='r',linestyle='--',marker='o',linewidth=3,label='Total Units')
+plt.title('Company profit per month')
+plt.xlabel('Month Number')
+plt.ylabel('Sold units number')
+plt.legend(loc = 'lower right')
+
+plt.show()
