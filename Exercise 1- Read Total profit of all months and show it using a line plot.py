a5b42e272a0c6c9266778383fa2281c7171a394a
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-03-11 01:04:14 +0530

    Create Exercise 1- Read Total profit of all months and show it using a line plot.py

---
+++ Exercise 1- Read Total profit of all months and show it using a line plot.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#importing dataset which is saved locally
+df = pd.read_csv('company_sales_data.csv')
+
+#Exercise 1
+#Total profit data provided for each month. Generated line plot must include the following properties: –
+
+# X label name = Month Number
+# Y label name = Total profit
+
+#Solution
+
+columns = ["month_number","facecream","facewash","toothpaste","bathingsoap","shampoo","moisturizer","total_units","total_profit"]
+
+
+#plotting the graph
+plt.figure(figsize=(5,5))
+
+plt.title('Company profit per month')
+plt.xlabel('Month Number')
+plt.ylabel('Total Profit')
+
+plt.plot(df['month_number'],df['total_profit'])
+
+plt.show()
