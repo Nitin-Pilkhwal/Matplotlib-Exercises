00e3df927657f6d47d1c2f395b4ff89dd0665f20
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-03-13 11:43:36 +0530

    Create Exercise 7- Read the total profit of each month and show it using the histogram to see the most common profit ranges.py

---
+++ Exercise 7- Read the total profit of each month and show it using the histogram to see the most common profit ranges.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+#importing dataset which is saved locally
+df = pd.read_csv('company_sales_data.csv')
+
+totalprofit = df['total_profit'].tolist()
+monthlist = df['month_number'].tolist()
+#Exercise 7
+
+# Exercise 7: Read the total profit of each month and show it using the histogram to see the most common profit ranges
+
+#Solution
+
+columns = ["month_number","facecream","facewash","toothpaste","bathingsoap","shampoo","moisturizer","total_units","total_profit"]
+
+# Plotting the graph
+plt.figure(figsize=(5,5))
+
+profit_range = [150000, 175000, 200000, 225000, 250000, 300000, 350000]
+plt.hist(totalprofit,profit_range,label='Profit Data')
+plt.legend()
+plt.show()
