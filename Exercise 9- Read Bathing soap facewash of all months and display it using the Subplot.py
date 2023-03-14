2f200cc0bdc133f74eed9d8ad6c0afaef5af4d3e
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-03-14 13:15:41 +0530

    Create Exercise 9- Read Bathing soap facewash of all months and display it using the Subplot.py

---
+++ Exercise 9- Read Bathing soap facewash of all months and display it using the Subplot.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+#importing dataset which is saved locally
+df = pd.read_csv('company_sales_data.csv')
+
+#Exercise 7
+
+# Exercise 7: Read the total profit of each month and show it using the histogram to see the most common profit ranges
+
+#Solution
+
+bathingsoap = df['bathingsoap'].tolist()
+facewash = df['facewash'].tolist()
+monthlist = df['month_number'].tolist()
+columns = ["month_number","facecream","facewash","toothpaste","bathingsoap","shampoo","moisturizer","total_units","total_profit"]
+
+# Plotting the graph
+# plt.figure(figsize=(5,5))
+plt.subplot(2,1,1)
+plt.title('Sales data of Bathing Soap')
+plt.plot(monthlist,bathingsoap,c='black',marker='o',linewidth=3)
+plt.subplot(2,1,2)
+plt.title('Sales data of facewash')
+plt.plot(monthlist,facewash,c='r',marker='o',linewidth=3)
+
+plt.xticks(monthlist)
+plt.ylabel('Sales unit in number')
+plt.xlabel('Month Number')
+plt.show()
